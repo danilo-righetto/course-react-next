c26bbcd455027a85838525e2a94bbd40fecce3d0
Danilo Righetto <drighetto@fcl.com.br>
2023-05-30 19:23:57 -0300

    Lifecicle methods - part 1

--- src/App.js
+++ src/App.js
@@ -21,16 +21,38 @@ class App extends Component {
       }
     ]
   }
+  timeoutUpdate = null;
+
+  componentDidMount() {
+    this.handleTimeout();
+  }
+
+  componentDidUpdate() {
+    this.handleTimeout();
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeoutUpdate);
+  }
+
+  handleTimeout = () => {
+    const { posts, counter } = this.state;
+    posts[0].title = 'O titulo mudou';
+
+    this.timeoutUpdate = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1});
+    }, 5000);
+  }
 
   render() {
     const { posts } = this.state;
     return (
       <div className="App">
         {posts.map(post => (
-          <>
-            <h1 key={post.id}>{post.title}</h1>
+          <div key={post.id}>
+            <h1>{post.title}</h1>
             <p>{post.body}</p>
-          </>
+          </div>
         ))}
       </div>
     );
